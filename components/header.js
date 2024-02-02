48c9ad4b6297b9089f272a32fcc3497360936ec7
dexterdesign23 <admin@dexterdesign.org>
2024-02-02 09:45:23 -0500

    File Structure Change, started adding content to index.html

--- components/header.js
+++ components/header.js
@@ -5,16 +5,15 @@ class Header extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-        <style>
-        </style>
+        <link href="../styles/headerstyles.css" rel="stylesheet">
         <header>
           <img src="../images/dexdesign_logo.png">
           <nav>
             <ul>
-              <li><a href="home.html">Home</a></li>
-              <li><a href="about.html">About</a></li>
-              <li><a href="projects.html">Projects</a></li>
-              <li><a href="contact.html">Contact</a></li>
+              <li><a href="../index.html">Home</a></li>
+              <li><a href="../about.html">About</a></li>
+              <li><a href="../projects.html">Projects</a></li>
+              <li><a href="../contact.html">Contact</a></li>
             </ul>
           </nav>
         </header>
