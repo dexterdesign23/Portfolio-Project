3f93be89c22a6782feed1b6dc7517d886b881e6c
dexterdesign23 <admin@dexterdesign.org>
2024-02-07 15:24:20 -0500

    Changed Homepage Wording & Added Stylish Buttons

--- components/header.js
+++ components/header.js
@@ -1,11 +1,10 @@
 class Header extends HTMLElement {
     constructor() {
       super();
-    }
-  
+    } 
     connectedCallback() {
-      this.innerHTML = `
-        <link href="../styles/headerstyles.css" rel="stylesheet">
+      this.innerHTML =`
+        <link href="../styles/header-styles.css" rel="stylesheet">
         <header>
           <img src="../images/dexdesign_logo.png">
           <nav>
@@ -15,6 +14,7 @@ class Header extends HTMLElement {
               <li><a href="../projects.html">Projects</a></li>
               <li><a href="../contact.html">Contact</a></li>
             </ul>
+            <p id="greeter"></p>
           </nav>
         </header>
       `;
